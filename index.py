b92faa08efcc5bae6282a2d5344200c8535f250e
Parag Naik <naikparag@gmail.com>
2019-01-14 16:25:47 +0530

    Admin: decorator for api key validation.

--- index.py
+++ index.py
@@ -9,11 +9,29 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_pyfile('flask.cfg')
 
+# ---- Decorator ----
+from functools import wraps
+from flask import request, abort
+def validate_apikey(func):
+    @wraps(func)
+    def decoratedFunc(*args, **kwargs):
+        print(request.headers.get('api-key'))
+        print(request.args.get('api-key'))
+        if (app.config['ADMIN_KEY'] == request.headers.get('api-key') 
+        or app.config['ADMIN_KEY'] == request.args.get('api-key')):
+            return(func(*args, **kwargs))
+        else:
+            abort(401)
+    return decoratedFunc
+
+# ---- Routes ----
 @app.route("/")
+@validate_apikey
 def index():
     return "Helllo NEWS-MAKER!"
 
 @app.route("/rss")
+@validate_apikey
 def rss():
     parsedData = rssParser.processRSSCsv()
     result = {
